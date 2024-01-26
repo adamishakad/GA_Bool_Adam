a6b86691aab7e514cc0e11eb8bdddabe6cf33545
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-26 10:58:20 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GA_Bool_Adam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isUserLoggedIn = true;
+
+            if (isUserLoggedIn)
+            {
+                // Code to execute when the user is logged in
+            }
+            else
+            {
+                // Code to execute when the user is not logged in
+                bool isStudent = true;
+                bool isTeenager = false;
+                Console.WriteLine("");
+
+
+
+            }
+
+
+
+
+
+        }
+    }
+
+}     
